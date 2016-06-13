5075f88e83d2a6e71103902ab690b6403a622bfa
Yingxiong <liyingxiong@live.com>
2016-06-13 15:55:45 +0200

    elastic mat

--- scratch/mdm/stress_strain_explorer.py
+++ scratch/mdm/stress_strain_explorer.py
@@ -16,15 +16,17 @@ if __name__ == '__main__':
     from ibvpy.mats.matsXD.matsXD_cmdm import \
         PhiFnStrainSoftening
 
-    phi_fn = PhiFnStrainSoftening(Epp=1e-4, Efp=2e-4, h=0.001)
-    mats_eval = MATS2DMicroplaneDamage(nu=0.3,
-                                       n_mp=30, phi_fn=phi_fn)
-    mats_eval = MATS2DElastic(E=30e+3)
+    import matplotlib.pyplot as plt
+
+#     phi_fn = PhiFnStrainSoftening(Epp=1e-4, Efp=2e-4, h=0.001)
+#     mats_eval = MATS2DMicroplaneDamage(nu=0.3,
+#                                        n_mp=30, phi_fn=phi_fn)
+    mats_eval = MATS2DElastic(E=30e+3, stress_state='plane_stress')
     stress_max = []
-    for alpha_rad in [0.0, np.pi / 8.0, np.pi / 4.0]:
+#     alpha_rad = np.pi / 4
+    for alpha_rad in [0., np.pi / 4]:
 
         explorer = MATSExplore(dim=MATS2DExplore(mats_eval=mats_eval))
-
         bc_proportional = explorer.tloop.tstepper.bcond_mngr.bcond_list[0]
         bc_proportional.alpha_rad = alpha_rad
 
@@ -33,12 +35,35 @@ if __name__ == '__main__':
         stress_strain = explorer.rtrace_mngr.rtrace_bound_list[0].trace
         strain = stress_strain.xdata
         stress = stress_strain.ydata
-        print 'strain', strain.shape
-        print strain
-        print 'stress', stress.shape
-        print stress
-        stress_argmax = np.argmax(stress)
+        plt.plot(strain, stress, marker='.', label='rad=' + str(alpha_rad))
+        plt.xlabel('strain')
+        plt.ylabel('stress')
+        plt.legend(loc='best')
 
-        max_stress = stress[stress_argmax]
-        print max_stress
-        stress_max.append(max_stress)
+        if alpha_rad == np.pi / 4:
+            plt.figure()
+            eps_eps = explorer.rtrace_mngr.rtrace_bound_list[1].trace
+            eps11 = eps_eps.xdata
+            eps22 = eps_eps.ydata
+            plt.plot(eps11, eps22, label='eps11-eps22')
+            plt.legend(loc='best')
+
+            plt.figure()
+            sig_sig = explorer.rtrace_mngr.rtrace_bound_list[2].trace
+            sig11 = sig_sig.xdata
+            sig22 = sig_sig.ydata
+            plt.plot(sig11, sig22, label='sig11-sig22')
+
+    D = explorer.tloop.tstepper.tse_integ.D_el
+
+    print np.dot(D, np.array([0.003 * np.sqrt(2) / 2, 0.003 * np.sqrt(2) / 2, 0]))
+
+    plt.legend(loc='best')
+    plt.show()
+
+
+#     stress_argmax = np.argmax(stress)
+#
+#     max_stress = stress[stress_argmax]
+#     print max_stress
+#     stress_max.append(max_stress)
